8e8ec441865560890c11e178aa676d93459ce217
Thanatoslayer6 <Thanatoslayer6@gmail.com>
2023-01-31 19:30:36 +0800

    Initial upload

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pet Feeder 9000',
+      themeMode: ThemeMode.dark,
+      // darkTheme: ThemeData.dark(),
+      home: RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pet Feeder 9000")),
+    );
+  }
+}
